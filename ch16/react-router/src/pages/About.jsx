34a3a240ede431a0959b43fd29312d2a0a65a584
mhb8436 <mhb8436@gmail.com>
2026-07-07 14:14:08 +0900

    router1

---
+++ ch16/react-router/src/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>
+                화면이 여러 개인 앱을 React Router로 분기하는 예제입니다.
+            </p>
+            <p className="muted">
+                메뉴를 눌러도 화면 전체를 새로 받아오지 않아요. 바뀌는 부분만 갈아 끼우므로 화면이 깜박이지 않아요.
+            </p>
+        </div>
+    );
+}
---
+++ ch16/react-router/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1>연락처</h1>
+            <p>궁금한 점이 있으면 아래로 연락을 주세요</p>
+            <ul>
+                <li>이메일 : hong@daishop.example</li>
+                <li>전화: 02-111-2222</li>
+                <li>운영시간 : 평일 09:30 ~ 16:10</li>
+            </ul>
+        </div>
+    );
+}
