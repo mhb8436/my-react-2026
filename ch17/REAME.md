6c8443f2290ce74bd169c1034df67cc69b2411c6
mhb8436 <mhb8436@gmail.com>
2026-07-08 12:03:01 +0900

    posts

--- ch17/REAME.md
+++ ch17/REAME.md
@@ -2,3 +2,10 @@ https://dhcihfnydvtnpsyvyqzz.supabase.co
 sb_publishable_IaA7awIgSW344VVJp4Axfw_6LMhAnFj
 
 ## https://github.com/mhb8436/my-react-2026/
+
+## 디렉토리 구조 
+src 
+    api -> rest endpoint와 통신 정용 코드 
+    components -> 화면에 필요한 코드 
+    hooks -> api 와 components를 연결하는 코드 
+    App.jsx   -> 진입지정 
---
+++ ch17/posts-api/src/api/posts.js
@@ -0,0 +1,20 @@
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPBASE_ANON_KEY;
+
+function headers(extra={}) {
+    return {
+        apikey: SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+        ...extra // 만약 필요한 헤더정보추가 
+    }
+}
+// 이 파일에는 서버에서 데이터를 가지고오고나, 서버에 데이터를 저장하는 역할만 모두 모았어요
+// 글 목록을 받아온다. 서버에서 글 목록을 가지고 오는 함수를 만들었어요
+export async function getPosts() {
+    const response = await fetch(
+        `${SUPABASE_URL}/rest/v1/posts`,
+        {headers: headers()}
+    )
+    if(!response.ok) throw new Error(`글 목록을 불러오지 못했어요`)
+    return response.json();
+}
