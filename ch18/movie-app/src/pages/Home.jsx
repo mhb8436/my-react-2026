de7b02f56bd04c028ed84b1012b2596a51bc7efd
mhb8436 <mhb8436@gmail.com>
2026-07-09 14:51:57 +0900

    home

--- ch18/movie-app/src/pages/Home.jsx
+++ ch18/movie-app/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import MovieGrid from '../components/MovieGrid';
 import SearchBar from '../components/SearchBar'
 import { useMovieSearch } from '../hooks/useMovieSearch'
 
@@ -5,9 +6,22 @@ export default function Home() {
     const {query, setQuery, results, loading, error, searched, search} = useMovieSearch();
     // useMovieSearch 훅에 비즈니스 + 및 화면 제어을 위한 필드가 모두 있어요
 
+    function renderResults() {
+        // jsx 반환 
+        if(loading) return <div>영화를 검색하는중..</div>
+        if(error) return <div>에러가 발생했어요</div>
+        return (
+            <MovieGrid
+                movies={results}
+                emptyText='검색 결과가 없습니다. 다른제목으로 찾아보세요'
+            />
+        )
+    }
+
     return (
         <div className='container'>
             <SearchBar value={query} onChange={setQuery} onSearch={search} />
-        </div>
+            {renderResults()}
+        </div>        
     )
 }
