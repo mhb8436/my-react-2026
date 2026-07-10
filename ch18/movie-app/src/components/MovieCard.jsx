5412294899df703fd1b706306bc9ab74171ec1d9
mhb8436 <mhb8436@gmail.com>
2026-07-10 10:59:43 +0900

    11

--- ch18/movie-app/src/components/MovieCard.jsx
+++ ch18/movie-app/src/components/MovieCard.jsx
@@ -1,12 +1,20 @@
 import {Link} from 'react-router-dom'
+import { useFavoritesContext } from '../context/FavoritesContext';
 
 export default function MovieCard({movie}) {
-
+    const { isFavorite, toggle } = useFavoritesContext();
     const hasPoster = movie.Poster && movie.Poster !== 'N/A';
-    const liked = false;
+    const liked = isFavorite(movie.imdbID);
+
     function handleHeart(e) {
         e.preventDefault(); // 카드가 링크로 감싸져 있어서, 상세화면으로 넘어가는것을 방지
-        e.stopPropagation(); // 클릭이 카드로 번지는것을 방지 
+        e.stopPropagation(); // 클릭이 카드로 번지는것을 방지
+        toggle({
+            imdbID: movie.imdbID,
+            Title: movie.Title,
+            Year: movie.Year,
+            Poster: movie.Poster
+        }); 
     }
     return (
         <Link to={`movie/${movie.imdbID}`} className='movie-card'>
