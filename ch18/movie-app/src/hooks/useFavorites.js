989e17592c3c19204e616cd7bf75949ceef3f161
mhb8436 <mhb8436@gmail.com>
2026-07-10 09:58:19 +0900

    1

--- ch18/movie-app/src/hooks/useFavorites.js
+++ ch18/movie-app/src/hooks/useFavorites.js
@@ -11,3 +11,14 @@ function readFromStorage () {
         return [];
     }
 }
+
+export function useFavorites() {
+    // readFromStorage : 함수명만 넘기면 리액트에서 이 컴포넌트의 첫번째 렌더에서
+    // 한번만 값을 가지고 옵니다. readFromStorage()  이 컴포넌트가 렌더될때 마다 호출됩니다. 
+    const [favorites, setFavorites] = useState(readFromStorage);
+
+    // favorites 가 바뀔 때 마다 localStroage에 업데이트를 합니다. 
+    useEffect(()=>{
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(favorites))
+    }, [favorites]); 
+}
