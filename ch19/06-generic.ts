20030fadd4fbbc6ce2ad21a91cf9c38e1b678530
mhb8436 <mhb8436@gmail.com>
2026-07-10 15:48:27 +0900

    11

--- ch19/06-generic.ts
+++ ch19/06-generic.ts
@@ -1,6 +1,8 @@
 // 인자로 받은 items 배열에서 첫번째 인자만 반환한다. 
+// T는 자리표시자 : 이 함수가 다루는 타입을 지칭하는 변수이름 
+// Type 약자 
 function first<T>(items: T[]) : T {
-    return items[0];
+    return items[0]; // K 타입 
 }
 
 const names : string[] = ['수진','민준','지우']
@@ -8,3 +10,12 @@ const prices : number[]  = [10000, 20000, 30000]
 
 const a = first<string>(names);
 console.log(a)
+const b = first<number>(prices);
+console.log(b)
+// const c = first<string>(prices);
+// useState<Product[]>([])
+// useState<string[]>([])
+// any 는 지양 , 가능하면 쓰지마세요
+function first1(items: any[]) { // java Object
+    return items[0]
+}
