cbb9fed6e64d467659822ab0e5368e63e23a15db
mhb8436 <mhb8436@gmail.com>
2026-07-10 11:39:48 +0900

    111

---
+++ ch18/movie-app/src/pages/Favorites.jsx
@@ -0,0 +1,7 @@
+import { Link } from 'react-router-dom';
+import MovieGrid from '../components/MovieGrid';
+import {useFavoritesContext} from '../context/FavoritesContext';
+
+export default function Favorites() {
+    
+}
