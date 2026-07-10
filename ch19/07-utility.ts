e47f23e7a4c924be176dde60e61ba6a09cff3388
mhb8436 <mhb8436@gmail.com>
2026-07-10 16:03:40 +0900

    111

---
+++ ch19/07-utility.ts
@@ -0,0 +1,30 @@
+type Category = 'electronics' | 'fashion' | 'book'
+
+interface Product { // 객체의 설계도 
+    id: string
+    name: string
+    price: number
+    category: Category
+}
+
+// Pick 유틸리티 함수 
+// 객체의 설계도 Product 에서 id와 name만 추출한 새로운 타입 생성
+type ProductPreview = Pick<Product, 'id' | 'name'>
+const previewProduct : ProductPreview = { 
+    id: 'p01', name:'키보드'
+}
+// console.log(previewProduct);
+
+// Omit 유틸리티 함수
+// id가 빠진 타입을 만들어주세요 
+type NewProduct = Omit<Product, "id">
+const draft :NewProduct = {name: "기계식키보드", price:100000, category:'electronics'}
+console.log(draft)
+
+// Partial 
+function updateProduct(id: string, patch: Partial<Product>) {
+    console.log(`$${id} 수정`, patch)
+}
+console.log(previewProduct, draft);
+updateProduct('p01', {price: 10000}); //  name, category
+console.log(previewProduct, draft);
