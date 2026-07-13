3ffeb7ce6dcd0b6c77bf1d17833a9ed66e1fc652
mhb8436 <mhb8436@gmail.com>
2026-07-13 11:11:13 +0900

    11

--- ch19/04-function.ts
+++ ch19/04-function.ts
@@ -16,10 +16,18 @@ console.log('20%할인 가격', formatPrice(discount(345100, 0.2)))
 // inStock 이라는 매개변수에 true 들어오면 '재고있음'을 반환해주고
 // isStock 에 false 들어오면 '재고없음'을 반환해주는 stockLabel 함수를
 // 만들어보세요 
-
+function stockLabel(inStock: boolean ) : string {
+    if(inStock) return "재고있음"
+    else return "재고없음"
+}
 // 연습문제 2
 // baseFree 기본값이 3000 원, shippingFee 함수를 만들어보세요 
 // extraKg(추가 kg)더해질 때마다 kg 당 500원씩 더합니다. 
+function shippingFee(baseFee: number = 3000, extraKg: number) {
+    return baseFee + extraKg * 500;
+}
 
+console.log('기본 배송비', shippingFee(3000, 0));
+console.log('오른 배송비', shippingFee(4000, 2));
 
 
