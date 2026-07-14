2c0c0f9880911ea1a8a22aa1222a4b176f360384
mhb8436 <mhb8436@gmail.com>
2026-07-14 12:08:56 +0900

    111

---
+++ ch20/my-shop/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import ProductGrid from '../components/ProductGrid'
+import { PRODUCTS } from '../data/products'
+import { useState } from 'react';
+import type {Category} from '../types'
+import CategoryFilter from '../components/CategoryFilter';
+import SearchBar from '../components/SearchBar';
+// ../ 부모디렉토리에서 찾아라
+// ./  현재디렉토리에서 찾아라
+
+export default function Home() {
+    const [query, setQuery] = useState('');
+    const [category, setCategory] = useState<Category | 'all'>('all');
+
+    const filtered = PRODUCTS.filter((p)=> (category === 'all' || p.category === category) && p.name.includes(query.trim()) );
+    return (
+        <div className='container'>
+        <div className='page-head'>
+            <h1>전체상품</h1>
+        </div>
+        <div className='toolbar'>
+            <SearchBar value={query} onChange={setQuery}/>
+            <CategoryFilter selected={category} onSelect={setCategory}/>
+        </div>
+        <ProductGrid products={filtered}/>
+        </div>
+    )
+}
