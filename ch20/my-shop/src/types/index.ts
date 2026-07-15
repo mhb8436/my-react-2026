fcbf5ad9d063c2f75015ca55b9bce5e7fb1261b7
mhb8436 <mhb8436@gmail.com>
2026-07-15 13:38:02 +0900

    11

--- ch20/my-shop/src/types/index.ts
+++ ch20/my-shop/src/types/index.ts
@@ -1,3 +1,5 @@
 export type { Product, Category } from './product'
 export {CATEGORY_LABELS} from './product';
 export type { CartItem, CartState, CartAction } from './cart';
+export type { Order, OrderStatus, ShippingInfo } from './order'
+export { ORDER_STATUS_LABELS } from './order'
