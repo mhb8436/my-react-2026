6dcb519a7376e6afd38bcbfa53227e8d99e05908
mhb8436 <mhb8436@gmail.com>
2026-07-15 15:07:12 +0900

    11

--- ch20/my-shop/src/hooks/useOrders.ts
+++ ch20/my-shop/src/hooks/useOrders.ts
@@ -25,6 +25,25 @@ export function useOrders() {
     return { orders, loading, error }
 }
 
-export function useOrder() {
+export function useOrder(id: string) {
+    const [order, setOrder] = useState<Order | null>(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null);
 
+    useEffect(()=>{
+        async function load() {
+            setLoading(true); setError(null);
+            try{
+                const data = await getOrder(id); //서버에서데이터를가지고온다.
+                setOrder(data)
+            }catch(e){
+                setError("서버에서 불러오지 못했어요")
+            }finally{
+                setLoading(false)
+            }
+        }
+        load();
+    }, []); // [] 이면 이컴포넌트가 처음 로딩될때 실행
+
+    return { order, loading, error }
 }
