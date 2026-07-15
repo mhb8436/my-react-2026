684c403643bb6e2f2b7c58a75c6e5711fd48c586
mhb8436 <mhb8436@gmail.com>
2026-07-15 11:25:42 +0900

    11

---
+++ ch20/my-shop/src/components/CartItemRow.tsx
@@ -0,0 +1,36 @@
+import type { CartItem } from '../types'
+import { formatPrice } from '../lib/format'
+import { useCart } from '../hooks/useCart'
+
+interface CartItemProps {
+    item: CartItem
+}
+
+export default function CartItem({item} : CartItemProps) {
+    const { setQuantity, removeItem } = useCart();
+    const {product, quantity} = item;
+
+    return (
+        <div className='cart-row'>
+            <img className='thumb' src={product.image}/>
+            <div className='info'>
+                <div className='p-name'>{product.name}</div>
+                <div className='p-price'>{formatPrice(product.price)}</div>
+            </div>
+            <div className='qty'>
+                <button 
+                    type="button"
+                    onClick={() => setQuantity(product.id, quantity - 1)}
+                    disabled={quantity <= 1}
+                > - </button>
+                <span>{quantity}</span>
+                <button
+                    type="button"
+                    onClick={() => setQuantity(product.id, quantity + 1)}
+                    disabled={quantity >= product.stock}
+                > + </button>
+            </div>
+            <div className='line-total'>{formatPrice(product.price * quantity)}</div>
+        </div>
+    )
+}
