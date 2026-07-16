6c98d06f4d4c4a0ea7f52d83743aca3f9c3e7c70
mhb8436 <mhb8436@gmail.com>
2026-07-16 11:08:46 +0900

    11

--- ch20/my-shop/src/App.tsx
+++ ch20/my-shop/src/App.tsx
@@ -9,22 +9,25 @@ import { CartProvider } from './context/CartContext';
 import Checkout from './pages/Checkout';
 import Orders from './pages/Orders';
 import OrderDetail from './pages/OrderDetail';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <CartProvider>
-      <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/" element={<Home/>} />
-          <Route path="/products/:id" element={<ProductDetail/>} />
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/checkout" element={<Checkout/>} />
-          <Route path="/orders" element={<Orders/>} />
-          <Route path="/orders/:id" element={<OrderDetail/>} />
-          <Route path="*" element={<NotFound/>} />
-        </Route>
-      </Routes>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Home/>} />
+            <Route path="/products/:id" element={<ProductDetail/>} />
+            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/checkout" element={<Checkout/>} />
+            <Route path="/orders" element={<Orders/>} />
+            <Route path="/orders/:id" element={<OrderDetail/>} />
+            <Route path="*" element={<NotFound/>} />
+          </Route>
+        </Routes>
+      </CartProvider>
+    </AuthProvider>
   )
 }
 
