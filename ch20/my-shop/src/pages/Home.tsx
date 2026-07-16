265c6ec8ef4b6ab4747cda1f58e721e8a0de1aa3
mhb8436 <mhb8436@gmail.com>
2026-07-16 14:06:29 +0900

    1

--- ch20/my-shop/src/pages/Home.tsx
+++ ch20/my-shop/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import type {Category} from '../types'
 import CategoryFilter from '../components/CategoryFilter';
 import SearchBar from '../components/SearchBar';
+import PromoBanners from '../components/PromoBanners';
 // ../ 부모디렉토리에서 찾아라
 // ./  현재디렉토리에서 찾아라
 
@@ -14,14 +15,15 @@ export default function Home() {
     const filtered = PRODUCTS.filter((p)=> (category === 'all' || p.category === category) && p.name.includes(query.trim()) );
     return (
         <div className='container'>
-        <div className='page-head'>
-            <h1>전체상품</h1>
-        </div>
-        <div className='toolbar'>
-            <SearchBar value={query} onChange={setQuery}/>
-            <CategoryFilter selected={category} onSelect={setCategory}/>
-        </div>
-        <ProductGrid products={filtered}/>
+            <PromoBanners/>
+            <div className='page-head'>
+                <h1>전체상품</h1>
+            </div>
+            <div className='toolbar'>
+                <SearchBar value={query} onChange={setQuery}/>
+                <CategoryFilter selected={category} onSelect={setCategory}/>
+            </div>
+            <ProductGrid products={filtered}/>
         </div>
     )
 }
