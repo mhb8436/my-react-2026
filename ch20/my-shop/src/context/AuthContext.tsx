d494f8d03ebb41b6f3c8753b6a1baa4dea6c3f22
mhb8436 <mhb8436@gmail.com>
2026-07-16 10:41:43 +0900

    11

---
+++ ch20/my-shop/src/context/AuthContext.tsx
@@ -0,0 +1,1 @@
+import { createContext, useContext, useState, type ReactNode} from 'react';
--- ch20/my-shop/src/types/index.ts
+++ ch20/my-shop/src/types/index.ts
@@ -3,3 +3,4 @@ export {CATEGORY_LABELS} from './product';
 export type { CartItem, CartState, CartAction } from './cart';
 export type { Order, OrderStatus, ShippingInfo } from './order'
 export { ORDER_STATUS_LABELS } from './order'
+export type { User } from './user';
